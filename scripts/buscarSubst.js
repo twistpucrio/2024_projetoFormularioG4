56b85d8c1ebee1316a8e0f15a51045218f4da429
judyFariaTecgraf <judyfaria@tecgraf.puc-rio.br>
2024-08-15 17:36:54 -0300

    funcao de validaçao LIMPAR

---
+++ scripts/buscarSubst.js
@@ -0,0 +1,27 @@
+
+function limpar(){
+    let texto = document.querySelector("#text")
+    alert(texto.value)
+
+    texto.value = ""
+}
+
+// Para colocar os eventos dos botões quando a página for carregada
+window.addEventListener("load", 
+    function(){
+        let btnLimpa = document.querySelector(".limpar")
+        let btnValid = document.querySelector(".submeter")
+
+        btnLimpa.addEventListener("click",
+            function(){
+                limpar();
+            }
+        )
+
+        btnValid.addEventListener("click", 
+            function(){
+                validar();
+            }
+        )
+    }
+)
