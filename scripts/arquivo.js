41e454207cc001251c2e04df6da4f5ec3d52f477
judyFariaTecgraf <judyfaria@tecgraf.puc-rio.br>
2024-08-20 15:54:30 -0300

    script limpar (checkbox) na pag arquivo

---
+++ scripts/arquivo.js
@@ -0,0 +1,30 @@
+function limpar(){
+    let atividade = document.getElementsByName("tipoArquivo")
+    
+    for (at of atividade){
+        at.checked = false
+    }
+    
+}
+
+
+// Para colocar os eventos dos botões quando a página for carregada
+window.addEventListener("load", 
+    function(){
+        let btnLimpa = document.querySelector(".limpar")
+        let btnValid = document.querySelector(".submeter")
+
+        
+        btnLimpa.addEventListener("click",
+            function(){
+                limpar();
+            }
+        )
+
+        btnValid.addEventListener("click", 
+            function(){
+                validar();
+            }
+        )
+    }
+)
