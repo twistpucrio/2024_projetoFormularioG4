d2f6f08a127d08737dc44dc4c7bfa62277996a77
HannaEpelboim <h4nn42005@gmail.com>
2024-08-20 17:23:27 -0300

    página de buscar e substituir funcionando

--- scripts/buscarSubst.js
+++ scripts/buscarSubst.js
@@ -1,64 +1,74 @@
 
 function limpar(){
-    let texto = document.querySelector("#text")
-    alert(texto.value)
+    let texto = document.querySelector("#text");
+    alert(texto.value);
 
-    texto.value = ""
+    texto.value = "";
     
 }
 
-function substitui(entrada, altera){
-    alert(entrada.replace(altera, "*******"));
+function substitui(entrada,remover,subst){
+
+    alert(entrada.replace(remover, subst));
 
     // exemplo com expressão regular
-    alert("Expressão Regular: " + entrada.replace(/\w/, "removido"));
+    // alert("Expressão Regular: " + entrada.replace(/\w/, "removido"));
 }
 
-function split(entrada, corte){
-
+function validar(){
+    let entrada, remover, subst;
+     
+    entrada = document.querySelector("#text");
+    remover = document.querySelector("#find");
+    subst = document.querySelector("#replace");
+    substitui(entrada,remover,subst);
 }
+
+// function split(entrada, corte){
+
+// }
 
 // Para colocar os eventos dos botões quando a página for carregada
 window.addEventListener("load", 
     function(){
-        let btnLimpa = document.querySelector(".limpar")
-        let btnValid = document.querySelector(".submeter")
+        let btnLimpa = document.querySelector(".limpar");
+        let btnValid = document.querySelector(".submeter");
 
-        let btnSplit = document.querySelector("#btnSplit")
-        let btnReplace = document.querySelector("#btnReplace")
+        // let btnSplit = document.querySelector("#btnSplit")
+        // let btnReplace = document.querySelector("#btnReplace")
 
-        let entrada, altera
+
 
         btnLimpa.addEventListener("click",
             function(){
                 limpar();
             }
-        )
+        );
 
         btnValid.addEventListener("click", 
             function(){
                 validar();
             }
-        )
+        );
 
-        btnSplit.addEventListener("click",
-            function(){
-                entrada = document.querySelector("#find")
-                altera = document.querySelector('#replace')
+        // btnSplit.addEventListener("click",
+        //     function(){
+        //         entrada = document.querySelector("#find");
+        //         altera = document.querySelector('#replace');
 
-                split(entrada, altera);
-            }
-        )
+        //         split(entrada, altera);
+        //     }
+        // )
 
-        btnReplace.addEventListener("click",
-            function(){
-                entrada = document.querySelector("#find")
-                altera = document.querySelector('#replace')
+        // btnReplace.addEventListener("click",
+        //     function(){
+        //         entrada = document.querySelector("#find");
+        //         altera = document.querySelector('#replace');
                 
-                substitui(entrada.value, altera.value);
-            }
-        )
+        //         substitui(entrada.value, altera.value);
+        //     }
+        // )
     }
-)
+);
 
 // contar -> match
