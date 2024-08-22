e062e7a3f11f5df1335c72a3160e3fa306e8788d
HannaEpelboim <h4nn42005@gmail.com>
2024-08-22 16:57:28 -0300

    inicio pg arq

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -98,35 +98,7 @@ function verificarTamanho(minTamanho, maxTamanho){
     
 }
 
-function validarExtensao(arquivo, extensao){
-    // obs: arquivo é o elemento input:file inteiro
 
-    /* PEGAR O NOME DO ARQUIVO */
-
-    // alert(arquivo.value)
-    caminho = arquivo.value.split("\\");
-
-    // alert(caminho)
-    alert("Primeira forma: " + caminho[ caminho.length - 1])
-
-    /* outra maneira: */
-    let resposta = arquivo.files[0];
-    alert("Segunda forma: " + resposta.name)
-
-    /* PEGAR A EXTENSAO */
-    // split no ponto 
-    let indice = caminho.length - 1;
-    let comparacao = caminho[indice].split(".");
-    let ext = comparacao[comparacao.length - 1];
-    alert(ext)
-
-    if(extensao.lower === ext.lower){
-        alert("igual")
-    }
-    else{
-        alert("diferente")
-    }
-}
 
 function validarSenha(senha){
 
