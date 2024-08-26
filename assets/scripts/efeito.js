15f36ba60bdc8df6520d1c43360d0bdb04b9708e
judyFariaTecgraf <judyfaria@tacgraf.puc-rio.br>
2024-08-26 08:51:18 -0300

    efeitoNavigator

---
+++ assets/scripts/efeito.js
@@ -0,0 +1,8 @@
+// var btnLink = document.querySelector(".a");
+// btnLink.onmousemove = function(e){
+//     var x = e.pageX - btnLink.offsetLeft;
+//     var y = e.pageY - btnLink.offsetTop;
+
+//     btnLink.style.setProperty('--eixoX', x + 'px')
+//     btnLink.style.setProperty('--eixoY', y + 'px')
+// }
